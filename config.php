6dadc02b2ca6945dc8577177658feafb518fcf67
isyamarshabindarputra <isyabindarp@gmail.com>
2025-01-13 11:31:36 +0700

    menambahkan koneksi

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+$host ="localhot";
+$username = "root";
+$password ="";
+$database="idekreatif";
+
+$conn = mysqli_connect($host,$username,$password,$database);
+
+if ($conn->connection_error){
+    die ("Databaese gagal terkoneksi :".$conn->connection_error);
+    
+}

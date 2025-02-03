04097eb2bc0458653039d180b04460b9077302eb
isyamarshabindarputra <isyabindarp@gmail.com>
2025-02-03 09:22:46 +0700

    init_session

--- .includes/init_session.php
+++ .includes/init_session.php
@@ -3,3 +3,8 @@ session_start();
 
 $name = $_SESSION['name'];
 $role = $_SESSION['role'];
+
+$notification = $_SESSION['notification'] ?? null ;
+if ($notification) {
+    unset($_SESSION['notification']);
+}

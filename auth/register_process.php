64fcb84f55617177437d8a118396bc5cfb9f476c
isyamarshabindarputra <isyabindarp@gmail.com>
2025-02-03 09:25:03 +0700

    register_prosess

--- auth/register_process.php
+++ auth/register_process.php
@@ -1,26 +1,29 @@
 <?php
 require_once("../config.php");
-sesseion_start();
+session_start();
+
  if ($_SERVER["REQUEST_METHOD"]=="POST"){
     $username = $_POST["username"];
     $name = $_POST["name"];
     $password =$_POST["password"];
     $hashedPassword =password_hash($password, PASSWORD_DEFAULT);
-    $sql ="INSERT INTO users (username, name ,password)VALUES('$username','$name','hashedPassword')";
-    if ($conn ->query($sql) ===TRUE){
+
+    $sql = "INSERT INTO users (username, name ,password)
+    VALUES ('$username','$name','hashedPassword')";
+    if ($conn->query($sql) ===TRUE) {
         $_SESSION['notification'] =[
             'type'=>'primary',
-            'massage' => 'Registasi Berasil!'
+            'message' => 'Registasi Berasil!'
         ];
     }else{
     $_SESSION['natification']= [
         'type' => 'denger',
-        'massage'=> 'gagal Registasi '.mysql_error($conn)
+        'message'=> 'gagal Registasi: ' . mysqli_error($conn)
     ];
     }
-    header('Location : login.php');
+    header('Location: login.php');
         exit();
-        //tes
+        //tes   
  }
  $conn->close();
  ?>

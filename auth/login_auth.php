12f0d4a81ace78c1795a0141286b46dff41b8b7e
isyamarshabindarputra <isyabindarp@gmail.com>
2025-02-03 09:24:18 +0700

    login_auth

--- auth/login_auth.php
+++ auth/login_auth.php
@@ -3,10 +3,10 @@ session_start();
 require_once("../config.php");
 if ($_SERVER["REQUEST_METHOD"]== "PSOT") {
     $username =$_POST["username"];
-    $password = =$_POST["password"];
+    $password =$_POST["password"];
 
     $sql ="SELECT * FROM users WHERE username='$username'";
-    $result =$conn->($sql);
+    $result = $conn->query($sql);
 
     if($result->num_rows >0) {
         $row = $result->fetch_assoc();
@@ -16,26 +16,27 @@ if ($_SERVER["REQUEST_METHOD"]== "PSOT") {
             $_SESSION["name"] =$row["name"];
             $_SESSION["role"] = $row["role"];
             $_SESSION["user_id"] = $row["user_id"];
-            $_SESSION['notification'] =[
+            $_SESSION['notification'] = [
                 'type' => 'primary',
                 'message' =>'selamat datang kembali!'
             ];
-            header('Location: ../dasbord.php');
+            header('location: ../dashboard.php');
             exit();
-            }else{
-                $_SESSION['notification'] =[
+        } else {
+                $_SESSION['notification'] = [
                     'type' => 'danger',
                     'message' => 'username atau password salah!'
             ];
-        }esle{
-            $_SESSION['notification'] =[
+        }
+        }else{
+            $_SESSION['notification'] = [
                 'type' => 'danger',
                     'message' => 'username atau password salah!'
             ];
         }
-        header('location: login.php');
+        header('Location: login.php');
         exit();
     }
     $conn->close();
-}
+
 ?>
